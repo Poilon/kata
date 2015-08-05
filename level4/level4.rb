fb772f95ac79c1dea9a9035909602fcfcaf00eb1
Pascal Knoth <pascal@knoth.fr>
2015-08-05 21:40:33 +0200

    level 4 done

--- level4/level4.rb
+++ level4/level4.rb
@@ -1,1 +1,28 @@
-#write code here
+require 'json'
+
+class SearchService
+	attr_accessor :dataset, :title, :description, :work_type
+	def initialize(params)
+		@dataset = params[:dataset]
+		@title = params[:title]
+		@description = params[:description]
+		@work_type = params[:work_type]
+	end
+
+	def call
+		res = []
+		File.open(dataset) do |f|
+			res = JSON.parse(f.read)
+		end
+
+		res.keep_if { |e| 
+			e['WorkType'] == work_type || 
+			e['JobTitle'].split(' ').include?(title) ||
+			e['JobSummary'].split(' ').include?(description)
+		}
+		res
+	end
+end
+
+p SearchService.new(dataset: 'job_offers.json', title: 'Laborer', description: 'Veterans', work_type: 'Federal').call
+
